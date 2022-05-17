5d0d1ff762099a3f99c0d23aa3ecfb0a9ef8c671
oktayudha05 <oktayudha05@gmail.com>
2022-05-17 18:47:16 +0700

    belajar proses proses dalam node

---
+++ javascript-projects/nodejs-basic/Process-object/materi.js
@@ -0,0 +1,19 @@
+//! Penggunaan process.memoryUsage() untuk mengetahui kinerja cpu
+// const cpuInformation = process.memoryUsage().heapUsed;
+// console.log(cpuInformation);
+
+//! Penggunaan process.env untuk menentukan nilai envirnent
+// console.log(process.env.NODE_ENV);
+
+//! Penggunaan process.argv[] untuk ngambil nilai di terminal
+// let firstName = process.argv[2];
+// let lastName = process.argv[3];
+ 
+// const jikaGaada = (namaNilai) => {
+//     if (namaNilai == undefined) {
+//         namaNilai = ""
+//     }
+//     return namaNilai;    
+// }
+
+// console.log(`Hello ${jikaGaada(firstName)} ${jikaGaada(lastName)}`);
