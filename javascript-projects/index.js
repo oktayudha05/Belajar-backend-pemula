349969f5a676ececf4c2ad3412c19ef503919f4d
oktayudha05 <oktayudha05@gmail.com>
2022-05-17 16:25:24 +0700

    belajar process.argv[]

--- javascript-projects/index.js
+++ javascript-projects/index.js
@@ -1,5 +1,18 @@
-const pesan = (nama) => {
-    console.log(`hallo ${nama}`);
+// const pesan = (nama) => {
+//     console.log(`hallo ${nama}`);
+// }
+
+// pesan('rio')
+
+//! Penggunaan process.argv[] untuk ngambil nilai di terminal
+let firstName = process.argv[2];
+let lastName = process.argv[3];
+ 
+const jikaGaada = (namaNilai) => {
+    if (namaNilai == undefined) {
+        namaNilai = ""
+    }
+    return namaNilai;
 }
 
-pesan('rio')
+console.log(`Hello ${jikaGaada(firstName)} ${jikaGaada(lastName)}`);
