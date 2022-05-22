29f8a69b565b86d9842bf3fc877764d10c2e976a
oktayudha05 <oktayudha05@gmail.com>
2022-05-22 21:42:35 +0700

    hapi-web-server-awal

---
+++ javascript-projects/hapi-web-server/routes.js
@@ -0,0 +1,54 @@
+const { handler } = require("@hapi/hapi/lib/cors");
+
+const routes = [
+    {
+        path : '/',
+        method : 'GET',
+        handler : (request, h) => {
+            return 'Homepage';
+        },
+    },
+    
+    {
+        path: '/',
+        method: '*',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+
+    {
+        path : '/about',
+        method : 'GET',
+        handler : (request, h) => {
+            return 'About Page';
+        },
+    },
+
+    {
+        path : '/about',
+        method : `*`,
+        handler : (request, h) => {
+            return 'halaman tidak dapat di akses dengan method tersebut';
+        },
+    },
+    
+    {
+        path : `/hello/{name?}`,
+        method : 'GET',
+        handler : (request, h) => {
+            const { name = `stranger` } = request.params;
+            return `hello ${name}`;
+        },
+    },
+
+    {
+        path : `/{any}`,
+        method : `*`,
+        handler : (request, h) => {
+            return 'halaman tidak di temukan';
+        },
+    },
+]
+
+module.exports = routes;
